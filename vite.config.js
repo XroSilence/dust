84d516668e2a26e3f0f7bdd42271a0d74bad12d5
XroSilence <86516127+XroSilence@users.noreply.github.com>
2024-12-09 01:47:43 -0800

    no place like home \m/...(^-^)...\m/

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/dust/',
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          html2canvas: ['html2canvas'],
-        },
-        chunkSizeWarningLimit: 500
+          vendor: ['react', 'react-dom']
+        }
       }
     }
   }
